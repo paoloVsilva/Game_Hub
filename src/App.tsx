b177df2b2e12a13a3261a073a3a991314b1fe557
Paolo Silva <paolo.silva@truefort.com>
2025-05-30 16:01:03 +0800

    Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { Button } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
 
 const App = () => {
   return (
-    <div>
-      <Button colorPalette='blue' variant='surface'>
-        Click Me
-      </Button>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        md: `"nav nav" "aside main"`
+      }}
+    >
+      <GridItem area='nav' bg='coral'>
+        Nav
+      </GridItem>
+      <GridItem area='aside' bg='gold' hideBelow='md'>
+        Aside
+      </GridItem>
+      <GridItem area='main' bg='dodgerblue'>
+        Main
+      </GridItem>
+    </Grid>
   )
 }
 
