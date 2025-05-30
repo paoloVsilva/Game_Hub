a4f427081cb7a12f60b6a1a9e69f2a99f08d4c03
Paolo Silva <paolo.silva@truefort.com>
2025-05-30 16:06:46 +0800

    Add navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem } from '@chakra-ui/react'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
@@ -8,8 +9,8 @@ const App = () => {
         md: `"nav nav" "aside main"`
       }}
     >
-      <GridItem area='nav' bg='coral'>
-        Nav
+      <GridItem area='nav'>
+        <NavBar />
       </GridItem>
       <GridItem area='aside' bg='gold' hideBelow='md'>
         Aside
