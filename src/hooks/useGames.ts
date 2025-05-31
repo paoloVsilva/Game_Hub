3d1c249da437be206dad7edc1729eb5ae023d246
Paolo Silva <paolo.silva@truefort.com>
2025-05-31 21:16:01 +0800

    Add critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,6 @@
-import { CanceledError } from "axios"
-import apiClient from "../services/api-client"
-import { useState, useEffect } from "react"
+import { CanceledError } from 'axios'
+import apiClient from '../services/api-client'
+import { useState, useEffect } from 'react'
 
 export interface Platform {
   id: number
@@ -12,6 +12,7 @@ export interface Game {
   name: string
   background_image: string
   parent_platforms: { platform: Platform }[]
+  metacritic: number
 }
 
 interface FetchGamesResponse {
@@ -21,14 +22,14 @@ interface FetchGamesResponse {
 
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([])
-  const [error, setError] = useState("")
+  const [error, setError] = useState('')
 
   useEffect(() => {
     const controller = new AbortController()
     apiClient
-      .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
-      .catch((err) => {
+      .get<FetchGamesResponse>('/games', { signal: controller.signal })
+      .then(res => setGames(res.data.results))
+      .catch(err => {
         if (err instanceof CanceledError) return
         setError(err.message)
       })
