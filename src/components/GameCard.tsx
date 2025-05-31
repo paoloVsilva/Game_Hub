912e6a23716843cdb8d315a70c980f9dd772c3fc
Paolo Silva <paolo.silva@truefort.com>
2025-05-31 21:39:29 +0800

    Remove duplicated styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card.Root borderRadius={10} overflow={'hidden'} width='300px'>
+    <Card.Root>
       <Image src={getCroppedImageURL(game.background_image)} />
       <Card.Body>
         <Heading fontSize={'2xl'}>{game.name}</Heading>
