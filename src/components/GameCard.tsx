e7362695a05edf4d5606158e490d4f349b72d51a
Paolo Silva <paolo.silva@truefort.com>
2025-05-31 21:34:47 +0800

    Show loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import { Card, Heading, HStack, Image, Text } from '@chakra-ui/react'
+import { Card, Heading, HStack, Image } from '@chakra-ui/react'
 import type { Game } from '../hooks/useGames'
 import PlatformIconList from './PlatformIconList'
 import CriticScore from './CriticScore'
@@ -10,7 +10,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card.Root borderRadius={10} overflow={'hidden'}>
+    <Card.Root borderRadius={10} overflow={'hidden'} width='300px'>
       <Image src={getCroppedImageURL(game.background_image)} />
       <Card.Body>
         <Heading fontSize={'2xl'}>{game.name}</Heading>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,18 +1,17 @@
 import useGames from '../hooks/useGames'
 import { SimpleGrid, Text } from '@chakra-ui/react'
 import GameCard from './GameCard'
+import GameCardSkeleton from './GameCardSkeleton'
 
 const GameGrid = () => {
-  const { games, error } = useGames()
+  const { games, error, isLoading } = useGames()
+  const skeletons = [...Array(10).keys()]
 
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        gap={10}
-        padding={10}
-      >
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} gap={10} padding={10}>
+        {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
         {games.map(game => (
           <GameCard key={game.id} game={game} />
         ))}
