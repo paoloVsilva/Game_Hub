5329936f8572307cf7b00d741edb6f4d0efe0755
paolosilva <silva.paolo.10@gmail.com>
2025-07-03 17:48:51 +0800

    Build game attribute

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,10 +1,14 @@
 import type { Platform } from '@/entities/Platform'
+import type { Genre } from './Genre'
+import type { Publisher } from './Publisher'
 
 export interface Game {
   description_raw: string
   id: number
   name: string
   slug: string
+  genres: Genre[]
+  publishers: Publisher[]
   background_image: string
   parent_platforms: { platform: Platform }[]
   metacritic: number
